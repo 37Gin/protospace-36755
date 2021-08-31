fb62f78137ff93ebf0aba7f6d815ff74992d15f0
37Gin <yujiterao4425@outlook.jp>
2021-08-31 10:52:24 +0900

    挙動修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,12 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
+    @prototype = Prototype.find(@comment.prototype_id)
+    @comments = @prototype.comments.includes(:user)
     if @comment.save
       redirect_to prototype_path(@comment.prototype_id)
     else
-      render prototype_path(@comment.prototype_id)
+      render template: "prototypes/show"
     end
   end
 
